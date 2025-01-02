9a2bede3e0248929f02f9ab44f00e0bf5d6cd124
Punisherjim <106792713+Punisherjim@users.noreply.github.com>
2025-01-03 06:48:38 +0800

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 {
   "name": "Math PayQuiz Game",
   "short_name": "PayQuiz",
-  "start_url": "/",
+  "start_url": "index.html",
   "display": "standalone",
   "background_color": "#ffffff",
   "theme_color": "#50A3A4",
